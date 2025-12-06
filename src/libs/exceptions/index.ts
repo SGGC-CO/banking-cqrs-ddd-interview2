c1305768795ac07c8eb046d4da71917c1de95a67
Nima <nima@gardoon.io>
2025-12-06 15:07:36 +0330

    add error normalization utilities for MongoDB; export mongo-error utility for improved error handling

--- src/libs/exceptions/index.ts
+++ src/libs/exceptions/index.ts
@@ -13,3 +13,6 @@ export * from './application.exceptions';
 // Global exception filter
 export * from './global-exception.filter';
 
+// Error normalization utilities (mappers)
+export * from './maps/mongo-error.util';
+
